13b9949ab755de9076352863689b793fd7ce97ce
Harshmishra1995 <harsh@logicloop.io>
2022-07-06 22:55:35 +0530

    Added Media Query Tutorials

--- nodejsPractice/index.js
+++ nodejsPractice/index.js
@@ -1,1 +1,15 @@
-console.log("hello")
+const express = require('express')
+
+const app = express()
+
+const port = 8001
+
+
+
+app.listen(port, (err) => {
+  if (err)
+  {
+    console.log('There is error in running the server',err)
+  }
+  console.log('The server is running at ',port)
+})
